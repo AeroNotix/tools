1c23e3c8704d4ea1845e5d86b4260fa9c3665d71
Aaron France <aaron.l.france@gmail.com>
2012-01-29 22:22:39 +0100

    Added qrc t ooo

--- qrc_creator.py
+++ qrc_creator.py
@@ -1,3 +1,41 @@
+#!/usr/bin/python2
+
 import argparse
+import optparse
+import os
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-dir', type=str)
+results = parser.parse_args()
+
+if results.dir is None:
+    print 'Usage:\n\tqrc_creator -dir "dirname"'
+    quit()
+
+if len(results.dir) > 1:
+
+    # create output file in same dir
+    qrc_file = open(os.path.join(results.dir, 'qrc_resource.qrc'), 'wb')
+
+    # start writing headers
+    qrc_file.write('<!DOCTYPE RCC><RCC version="1.0">\n')
+    qrc_file.write('<qresource>\n')
+
+    # iterate through files, only files with extensions
+    # get used and html files get treat differently
+    try:
+        for file in os.listdir(results.dir):
+
+            fname = file.split('.')[0]            
+            if file.split('.')[1] == 'html':
+                qrc_file.write('<file alias={0}>{1}</file>\n'.format(fname, file))
+            else:
+                qrc_file.write('<file alias="{0}">{1}</file>\n'.format(fname, file))
+                
+    except IndexError:
+        pass
+        
+    qrc_file.write('</qresource>\n')
+    qrc_file.write('</RCC>\n')
+
+
